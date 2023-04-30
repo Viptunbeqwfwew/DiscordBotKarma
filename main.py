6e85add210961bf8860a9eb7f893762cf2520f29
Nybashirka <ilabon60@gmail.com>
2023-05-01 05:24:38 +0900

    Заработала функция определение плохих слов.

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 import discord
 import sqlite3
 import re
-import argparse
 from setting import TOKEN
 
 
-forbidden_words = {}
+forbidden_words = [r"бл[я\*]ть"]
 
 
 class KarmaBot(discord.Client):
     async def on_ready(self):
         print("Бот встал на стражу порядка кармы сервера.")
+
+    def punishment(self, patern):
+        pass
+
+    async def on_message(self, message: discord.Message):
+        if message.author == self.user:
+            return
+        for i in forbidden_words:
+            regex = re.compile(i, re.IGNORECASE)
+            if regex.search(message.content):
+                punishment(message.content)
+                await message.channel.send("Вы, " + message.author + ", понизили свою карму")
 
 
 
